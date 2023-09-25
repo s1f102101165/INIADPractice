c6ffcbceb3c89eee1ae46c34717245407512e27f
yamagishiTeamA <85399300+s1f102101154@users.noreply.github.com>
2023-09-25 23:33:00 +0900

    クラスタリング関数を変更
    結果変わらず

--- chatapp/views.py
+++ chatapp/views.py
@@ -4,20 +4,23 @@ from django.http import JsonResponse
 from chatapp.models import Comments, User
 import requests
 from collections import defaultdict
-from gensim.models.keyedvectors import KeyedVectors
-from sklearn.cluster import KMeans
 import json
+from sklearn.cluster import KMeans
 import numpy as np
-from scipy.cluster.hierarchy import linkage, fcluster
-from collections import defaultdict
+from gensim.models import Word2Vec
+
+#learning.pyをimport
+from learning import *
+
 #ここにGoogle Cloud Platformで入手したYoutubeDataAPIをそのまま入力
 YT_API_KEY = "AIzaSyDpJkQseYjIeAA_9j2vUzY0qxK_c5ZvwoU"
 # Word2Vecモデルを読み込む
 word2vec_model_path = 'youtube_comments_model.model'
 # 単語数の上限を設定
 max_vocab = 10000
-# クラスタ数を設定
+
 n_clusters = 5
+
 #==========☆ トップページ用関数 ☆==========
 def index(request):
     return render(request, "chatapp/index.html")
@@ -25,58 +28,42 @@ def index(request):
 # 動作確認用に一時的に作ったページです。
 def getchattest(request):
     return render(request, "chatapp/getchattest.html")
-# 文章のベクトルを計算するための関数
-def sentence_vector(sentence, model):
-    # 文章をスペースで分割し、各単語がWord2Vecモデルの語彙に存在するか確認
-    words = [word for word in sentence.split() if word in model.wv.key_to_index]
-    # もし文章にWord2Vecの語彙に存在する単語が一つもなければNoneを返す
-    if len(words) == 0:
-        return None
-    # 存在する単語のベクトルの平均を計算して返す
-    return sum(model.wv[word] for word in words) / len(words)
-def cluster_data(data, word2vec_model_path, n_clusters):
-    print("コメント:",data)
-    # 各コメントのベクトルを計算前に、dataの要素数を確認
-    print("変換前のコメント数:",len(data))
-    # Word2Vecモデルをロード
-    model = KeyedVectors.load(word2vec_model_path)
-    print("モデルの単語数:",len(model.wv.index_to_key))
-    # modelの確認
-    #print(model.wv.index_to_key)
-    
-    """# 各コメントのベクトルを計算
-    vectors = [sentence_vector(comment, model) for comment in data]
-    # Noneを除外（Word2Vecの語彙に存在しないコメントを除外）
-    vectors = [v for v in vectors if v is not None]"""
-    
-     # 各コメントのベクトルを計算
-    vectors = []
-    converted_comments = []
-    for comment in data:
-        vector = sentence_vector(comment, model)
-        if vector is not None:
-            vectors.append(vector)
-            converted_comments.append(comment)
-    
-    vectors = np.array(vectors)
-    if len(vectors) < 2:
-        print("ベクトルが足りません")
 
-
-    # 階層的クラスタリングを適用
-    Z = linkage(vectors, method='ward', metric='euclidean')
+def vectorize_comments(comments, model):
+    vectorized_comments = []
     
-    # クラスタリングの結果を取得
-    cluster_labels = fcluster(Z, n_clusters, criterion='maxclust')
-
-    # 各クラスタに所属するコメントを集めるための辞書
-    cluster_to_comments = defaultdict(list)
-
-    # クラスタのラベルとコメントを対応付けて辞書に追加
-    for cluster_id, comment in zip(cluster_labels, converted_comments):
-        cluster_to_comments[cluster_id].append(comment)
+    for comment in comments:
+        words = comment.split()
+        vectors = [model.wv[word] for word in words if word in model.wv]
+        
+        if vectors:
+            vectorized_comments.append(np.mean(vectors, axis=0))
+        else:
+            vectorized_comments.append(np.zeros(model.vector_size))
     
-    return cluster_to_comments
+    return vectorized_comments
+
+def cluster_data(data, word2vec_model_path, n_clusters=n_clusters):
+    # Word2Vecモデルのロード
+    model = Word2Vec.load(word2vec_model_path)
+    
+    # 各コメントをベクトルに変換
+    X = vectorize_comments(data, model)
+
+    # 実際のクラスタリング
+    km_model = KMeans(n_clusters=n_clusters)
+    if len(X) == 0:
+        return []
+    km_model.fit(X)
+    labels = km_model.labels_
+
+    # クラスタごとにコメントを格納するリスト
+    clustered_comments_list = [[] for _ in range(n_clusters)]
+    for idx, label in enumerate(labels):
+        clustered_comments_list[label].append(data[idx])
+
+    return clustered_comments_list
+
 # コメント取得をテストページで使えるようにAPI化したもの
 def api_getchat(request):
     # トークンを取得
@@ -118,6 +105,17 @@ def get_chat(video_id, pageToken, api_key):
         params['pageToken'] = pageToken
     # YouTube APIを呼び出し、結果をJSONとして取得
     response_data = requests.get(url, params=params).json()
+
+    # APIの応答に 'error' キーが存在する場合、エラーハンドリングを行う
+    if 'error' in response_data:
+        print("Error from YouTube API:", response_data['error']['message'])
+        # 必要に応じて、その他のエラーハンドリング処理を追加
+        return []  # ここでは空のリストを返すことを例としています
+
+    # エラーがない場合、通常の処理を続行
+    comments = [item["snippet"]["displayMessage"] for item in response_data["items"]]
+    print("なかみ",response_data)
+
     # 取得したデータからコメントの内容のみをリストとして抽出
     comments = [item["snippet"]["displayMessage"] for item in response_data["items"]]
     
