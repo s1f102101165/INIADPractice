bb7cec49af6d31bafdbe6c38b956f290405728ab
yamagishiTeamA <85399300+s1f102101154@users.noreply.github.com>
2023-10-07 10:44:01 +0900

    クラスタリング
    3つのクラスに分類、それぞれからひとつずつコメントを表示

--- chatapp/views.py
+++ chatapp/views.py
@@ -4,18 +4,19 @@ from django.http import JsonResponse
 from chatapp.models import Comments, User
 import requests
 from collections import defaultdict
-from gensim.models.keyedvectors import KeyedVectors
-from sklearn.cluster import KMeans
 import json
+from sklearn.cluster import KMeans
 import numpy as np
+from gensim.models import FastText
+import fasttext
+import os
+import MeCab
 #ここにGoogle Cloud Platformで入手したYoutubeDataAPIをそのまま入力
-YT_API_KEY = ""
-# Word2Vecモデルを読み込む
-word2vec_model_path = 'youtube_comments_model.model'
-# 単語数の上限を設定
-max_vocab = 10000
-# クラスタ数を設定
-n_clusters = 5
+YT_API_KEY = "AIzaSyDpJkQseYjIeAA_9j2vUzY0qxK_c5ZvwoU"
+# モデルを読み込む
+model_path = "crawl-300d-2M-subword_part_aa"
+fasttext_model = fasttext.load_model(model_path)
+n_clusters = 3
 #==========☆ トップページ用関数 ☆==========
 def index(request):
     return render(request, "chatapp/index.html")
@@ -23,70 +24,32 @@ def index(request):
 # 動作確認用に一時的に作ったページです。
 def getchattest(request):
     return render(request, "chatapp/getchattest.html")
-# 文章のベクトルを計算するための関数
-def sentence_vector(sentence, model):
-    # 文章をスペースで分割し、各単語がWord2Vecモデルの語彙に存在するか確認
-    words = [word for word in sentence.split() if word in model.wv.key_to_index]
-    # もし文章にWord2Vecの語彙に存在する単語が一つもなければNoneを返す
-    if len(words) == 0:
-        return None
-    # 存在する単語のベクトルの平均を計算して返す
-    return sum(model.wv[word] for word in words) / len(words)
-def cluster_data(data, word2vec_model_path, n_clusters):
-    print("コメント:",data)
-    # 各コメントのベクトルを計算前に、dataの要素数を確認
-    print("変換前のコメント数:",len(data))
-    # Word2Vecモデルをロード
-    model = KeyedVectors.load(word2vec_model_path)
-    print("モデルの単語数:",len(model.wv.index_to_key))
-    # modelの確認
-    #print(model.wv.index_to_key)
+def cluster_data(comments, fasttext_model):
+    vectorized_comments = []
+    vector_dim = fasttext_model.get_dimension()  # ベクトルの次元数を取得
+    tagger = MeCab.Tagger("-Owakati") # 文字列を単語に区切るルールを指定
+    for comment in comments:
+        space_sentence = tagger.parse(comment) # コメントの文字列を単語ごとにスペースで区切る
+        words = space_sentence.split()  # 簡単にスペースで単語を分割
+        vectors = [fasttext_model.get_word_vector(word) for word in words]
+        if vectors:
+            avg_vector = sum(vectors) / len(vectors)
+        else:
+            avg_vector = [0] * vector_dim
+        vectorized_comments.append(avg_vector)
     
-    """# 各コメントのベクトルを計算
-    vectors = [sentence_vector(comment, model) for comment in data]
-    # Noneを除外（Word2Vecの語彙に存在しないコメントを除外）
-    vectors = [v for v in vectors if v is not None]"""
-    
-    # 各コメントのベクトルを計算
-    vectors = []
-    converted_comments = []
-    for comment in data:
-        vector = sentence_vector(comment, model)
-        if vector is not None:
-            vectors.append(vector)
-            converted_comments.append(comment)
-    
-    vectors = np.array(vectors)
-    #print("vectorsの形状:",vectors)
-    print("変換後のコメント数:",len(vectors))
-    print("変換後のコメントの中身:")
-    for comment in converted_comments:
-        print(comment)
-    # vectorsが空の場合、処理を終了
-    if len(vectors) == 0:
-        print("No data to cluster")
-        return {}
-    # vectorsが1次元の場合、2次元にreshape
-    if len(vectors.shape) == 1:
-        vectors = vectors.reshape(-1, 1)
-    
-    # KMeansクラスタリングモデルを初期化
-    kmeans_model = KMeans(n_clusters=min(n_clusters, len(vectors)), n_init=10, verbose=1, random_state=42)
-    # ベクトルに対してクラスタリングを実行
-    kmeans_model.fit(vectors)
-    
-    # 各ベクトルが所属するクラスタのラベルを取得
-    cluster_labels = kmeans_model.labels_
-    
-    # 各クラスタに所属するコメントを集めるための辞書
-    cluster_to_comments = defaultdict(list)
-    # クラスタのラベルとコメントを対応付けて辞書に追加
-    for cluster_id, comment in zip(cluster_labels, converted_comments):
-        cluster_to_comments[cluster_id].append(comment)
-    
-    #print("クラスタごとのコメントリスト：",cluster_to_comments)
-    # クラスタ毎のコメントリストを返す
-    return cluster_to_comments
+    if vectorized_comments:  # vectorized_commentsが空でないことを確認
+        kmeans = KMeans(n_clusters=min(n_clusters, len(vectors)), n_init=10, verbose=1, random_state=42).fit(vectorized_comments)
+    else:
+        print("No comments were vectorized.")
+    labels = kmeans.labels_
+    print("[", labels, "]")
+    clustered_comments = {}
+    for i, label in enumerate(labels):
+        if label not in clustered_comments:
+            clustered_comments[label] = []
+        clustered_comments[label].append(comments[i])
+    return clustered_comments
 # コメント取得をテストページで使えるようにAPI化したもの
 def api_getchat(request):
     # トークンを取得
@@ -128,12 +91,18 @@ def get_chat(video_id, pageToken, api_key):
         params['pageToken'] = pageToken
     # YouTube APIを呼び出し、結果をJSONとして取得
     response_data = requests.get(url, params=params).json()
+    # APIの応答に 'error' キーが存在する場合、エラーハンドリングを行う
+    if 'error' in response_data:
+        print("Error from YouTube API:", response_data['error']['message'])
+        # 必要に応じて、その他のエラーハンドリング処理を追加
+        return []  # ここでは空のリストを返すことを例としています
+    # エラーがない場合、通常の処理を続行
     # 取得したデータからコメントの内容のみをリストとして抽出
     comments = [item["snippet"]["displayMessage"] for item in response_data["items"]]
-    
+    print("なかみ",comments)
     # 抽出したコメントをクラスタリング
-    clustered_comments = cluster_data(comments, word2vec_model_path, n_clusters)
-    
+    clustered_comments = cluster_data(comments, fasttext_model)  
+    print("クラスタリング:",clustered_comments)
     # nextPageTokenを設定
     userobj = User.objects.get(pk=1)
     userobj.nextPageToken = response_data["nextPageToken"]
@@ -172,8 +141,6 @@ def input_database(data):
         comment = Comments(body = new_body, posted_at = new_posted_at)
         comment.save()
     return
-
-
 # 【今は使っていないですが後で使うかもしれないので一応残しておきます】旧ver 取得したコメントをデータベースに格納する関数
 def input_old_database(data):
     if ("pageInfo" in data):
