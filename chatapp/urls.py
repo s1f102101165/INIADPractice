7006455514617f48734cedee18f35496519ec067
s1f102102807 <s1f102102807@iniad.org>
2023-11-22 15:47:15 +0900

    HTML/CSS/JAVASCRIPT実装

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('chat', views.chat, name='chat'), 
     path('getchattest', views.view_comments, name='getchattest'), 
+
     
     path('api/getchatapi/', views.api_getchat),
     path('api/resetapi/', views.api_reset),
