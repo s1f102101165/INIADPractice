72f0463550a6efeed195034abc61d6ffc2af0587
yamada <s1F102101165@iniad.org>
2023-12-13 14:38:37 +0900

    アンチコメント判定結果の値そのものも保存

--- chatapp/models.py
+++ chatapp/models.py
@@ -10,8 +10,8 @@ class Comments(models.Model):
     posted_at = models.DateTimeField() #コメント投稿日時（日本時間に合わせるには+9時間）
     cluster_label = models.IntegerField(default=-1) #クラスター番号。おそらく表示したり使ったりはしないけど一応保存しとく
     cluster_display = models.BooleanField(default=False) #クラスタリングした結果、表示するならTrueに（0~3の各分類からそれぞれの代表コメントを1つずつ選んでtrueにする）
-    anti_label = models.BooleanField(default=False) #アンチコメントかどうか
-    anti_display = models.BooleanField(default=False) #アンチコメントかどうか
+    anti_violence_score = models.FloatField(default=0.0) #アンチコメントかどうかの値
+    anti_display = models.BooleanField(default=False) #アンチコメントかどうかTrueFalse
     
     def __str__(self):
         return self.body
